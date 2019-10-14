c8eff65a4336efc87d7a26ced83e5c7c119e0ef1
Michael Harmon <mwharmon1@dmacc.edu>
2019-10-14 09:57:50 -0500

    Added try except for key error to handle invalid keys passed to function. All tests are passing.

--- main_program/assign_average.py
+++ main_program/assign_average.py
@@ -12,16 +12,19 @@ def switch_average(key_to_find):
     :param key_to_find: key that is passed in
     :return: value of key if found, if not found return message
     """
-    return {
-        'A': 100,
-        'B': 89,
-        'C': 79,
-        'D': 69,
-        'F': 59,
-    }[key_to_find]
-    # pass
+    key_not_found = 'Value not found for ' + str(key_to_find) + '.'
+    try:
+        return {
+            'A': 100,
+            'B': 89,
+            'C': 79,
+            'D': 69,
+            'F': 59,
+        }[key_to_find]
+    except KeyError:
+        return key_not_found
 
 
 if __name__ == '__main__':
-    key = 'B'
+    key = '$'
     print(switch_average(key))
